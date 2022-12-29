6511da2a1d69c84e12b166a82ef185c62e4f4cf5
gdiguglielmo <gusbertus@gmail.com>
2022-12-29 18:02:26 +0100

    added facebook pixel

--- site/snippets/header.php
+++ site/snippets/header.php
@@ -13,6 +13,7 @@
     <!-- Für Windows Metro -->
     <meta name="msapplication-square310x310logo" content="/favicon/mstile-310x310.png">
     <meta name="msapplication-TileColor" content="[HEXFARBE (z.B. #000000)]">
+    <meta name="facebook-domain-verification" content="xs4wurd5wqz7d32tzrlsaxwm07205s" />
     <script>
     document.documentElement.className = "js";
     var supportsCssVars = function() {
@@ -36,8 +37,20 @@
         );
     </script>
     <script>
-    document.getElementsByTagName("html")[0].className += " js";
+    !function(f,b,e,v,n,t,s)
+    {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+    n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+    if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+    n.queue=[];t=b.createElement(e);t.async=!0;
+    t.src=v;s=b.getElementsByTagName(e)[0];
+    s.parentNode.insertBefore(t,s)}(window,document,'script',
+    'https://connect.facebook.net/en_US/fbevents.js');
+    fbq('init', '639324087794593'); 
+    fbq('track', 'PageView');
     </script>
+    <noscript>
+        <img height="1" width="1" src="https://www.facebook.com/tr?id=639324087794593&ev=PageView&noscript=1"/>
+    </noscript>
     <link rel="stylesheet" href="https://unpkg.com/swiper@7/swiper-bundle.min.css" />
     <?= Bnomei\Fingerprint::css('assets/css/style.css');?>
     <title><?= $site->title()?>
