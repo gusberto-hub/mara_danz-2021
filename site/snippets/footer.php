47e6968dd7aab55848dd1012f5fac89d90b1be5a
gdiguglielmo <gusbertus@gmail.com>
2023-07-19 08:41:23 +0200

    added dynamic snipcart key

--- site/snippets/footer.php
+++ site/snippets/footer.php
@@ -4,10 +4,21 @@
 
 <?= Bnomei\Fingerprint::js('assets/js/scripts.min.js'); ?>
 
+<?php
+$snipcart_api = '';
+
+if ($_SERVER['SERVER_NAME'] === 'maradanz.com') :
+    $snipcart_api = "YmE3ZWFlMjQtY2I0OC00YjkxLTkzZGItYWJlMDhhZDIxM2U4NjM3NjMyMzk2OTU0NTg5Mzgx";
+else :
+    $snipcart_api = "N2JlNTg4M2UtMmVlNy00NGI0LWE4YmMtNmZhZTBhYmU1OGE4NjM3NjMyMzk2OTU0NTg5Mzgx";
+endif
+?>
+
 <?php if (page('shop')->isOpen()) : ?>
     <script async src="https://cdn.snipcart.com/themes/v3.2.0/default/snipcart.js"></script>
-    <div id="snipcart" data-config-modal-style="side" data-api-key="YmE3ZWFlMjQtY2I0OC00YjkxLTkzZGItYWJlMDhhZDIxM2U4NjM3NjMyMzk2OTU0NTg5Mzgx" hidden></div>
+    <div id="snipcart" data-config-modal-style="side" data-api-key=<?= $snipcart_api ?> hidden></div>
 <?php endif ?>
+<p style="display: none;"><?= $_SERVER['SERVER_NAME'] ?></p>
 </body>
 
 </html>
