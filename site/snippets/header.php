6d46cad76405d728f78d0b04138a5c3405ec185e
Giuseppe Di Guglielmo <giuseppe.diguglielmo@guavestudios.com>
2026-02-22 21:31:47 +0100

    Add robots meta tag for non-production environments

--- site/snippets/header.php
+++ site/snippets/header.php
@@ -5,6 +5,10 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, viewport-fit=cover">
     <meta name="description" content="<?= $site->metaDescription() ?>">
+    <?php if (kirby()->environment()->host() !== 'maradanz.com'): ?>
+    <meta name="robots" content="noindex, nofollow">
+    <?php endif ?>
+    
     <!-- Für Apple-Geräte -->
     <link rel="apple-touch-icon" sizes="180x180" href="/favicon/apple-touch-icon-180x180.png">
     <!-- Für Browser -->
@@ -14,28 +18,6 @@
     <meta name="msapplication-square310x310logo" content="/favicon/mstile-310x310.png">
     <meta name="msapplication-TileColor" content="[HEXFARBE (z.B. #000000)]">
     <meta name="facebook-domain-verification" content="xs4wurd5wqz7d32tzrlsaxwm07205s" />
-    <script>
-        document.documentElement.className = "js";
-        var supportsCssVars = function() {
-            var e,
-                t = document.createElement("style");
-            return (
-                (t.innerHTML = "root: { --tmp-var: bold; }"),
-                document.head.appendChild(t),
-                (e = !!(
-                    window.CSS &&
-                    window.CSS.supports &&
-                    window.CSS.supports("font-weight", "var(--tmp-var)")
-                )),
-                t.parentNode.removeChild(t),
-                e
-            );
-        };
-        supportsCssVars() ||
-            alert(
-                "Please view this demo in a modern browser that supports CSS Variables."
-            );
-    </script>
     <script>
         ! function(f, b, e, v, n, t, s) {
             if (f.fbq) return;
