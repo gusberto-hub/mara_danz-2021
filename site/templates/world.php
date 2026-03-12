5bb7eae88e98268612a9fe4142693b4bc2728278
Giuseppe Di Guglielmo <giuseppe.diguglielmo@guavestudios.com>
2026-03-12 08:23:12 +0100

    refactor: restructure world page, remove reloved section

--- site/templates/world.php
+++ site/templates/world.php
@@ -1,52 +1,25 @@
 <?php snippet('layout', slots: true) ?>
 <?php slot() ?>
 
-<header>
-    <div class="sub-menu">
-        <ul>
-            <li><a href="#<?= $page->headline1() ?>" class=" js-smooth-scroll"><?= $page->headline1() ?></a>
-            </li>
-            <li><a href="#<?= $page->headline2() ?>" class=" js-smooth-scroll"><?= $page->headline2() ?></a>
-            </li>
-        </ul>
-    </div>
-</header>
-<div class="main-container" id="world">
-    <section>
-        <article id="<?= $page->headline1() ?>">
-            <div class="headline">
-                <?= $site->file('danz_typo.svg') ?>
+<div class="main-container world-page">
+    <section class="world-page__section">
+        <div class="world-page__headline">
+            <?= $site->file('danz_typo.png') ?>
+        </div>
+        <div class="world-page__paragraph">
+            <?= $page->paragraph1()->kirbytext() ?>
+            <div class="info-box__button">
+                <button>
+                    Contact
+                </button>
             </div>
-            <div class="paragraph">
-                <?= $page->paragraph1()->kirbytext() ?>
-                <div class="info-button">
-                    <button>
-                        Contact
-                    </button>
-                </div>
-            </div>
-            <div class="static-image">
-                <?= $page->Picture_static_1()->toFile() ?>
-            </div>
-            <div class="flow-image">
-                <?= $page->Picture_flow_1()->toFile() ?>
-            </div>
-        </article>
-
-        <article id="<?= $page->headline2() ?>">
-            <div class="headline">
-                <?= $site->file('reloved_typo.svg') ?>
-            </div>
-            <div class="paragraph">
-                <?= $page->paragraph2()->kirbytext() ?>
-            </div>
-            <div class="static-image">
-                <?= $page->Picture_static_2()->toFile() ?>
-            </div>
-            <div class="flow-image reloved">
-                <?= $page->Picture_flow_2()->toFile() ?>
-            </div>
-        </article>
+        </div>
+        <div class="world-page__static-image">
+            <?= $page->Picture_static_1()->toFile() ?>
+        </div>
+        <div class="world-page__flow-image">
+            <?= $page->Picture_flow_1()->toFile() ?>
+        </div>
     </section>
 </div>
 
